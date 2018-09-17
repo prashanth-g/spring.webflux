9057669ce7dfd2f34641101e402024d03ed27a53
prashanth-g <prashanthcsgce@gmail.com>
2018-09-17 23:25:32 +0530

    Added SpringWebFlux01

---
+++ src/main/java/com/prashanth/spring/webflux/init/SpringWebFlux01.java
@@ -0,0 +1,13 @@
+package com.prashanth.spring.webflux.init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFlux01 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFlux01.class, args);
+    }
+
+}
